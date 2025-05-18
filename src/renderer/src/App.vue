8c6ac928a6c0044dded0ea9da58f17e6dd7a5479
mishashto <me@reze.moe>
2025-05-18 11:59:33 +0900

    initial commit

---
+++ src/renderer/src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import Topbar from './components/Topbar.vue'
+
+const ipcHandle = (): void => window.electron.ipcRenderer.send('ping')
+</script>
+
+<template>
+  <Topbar />
+</template>
---
+++ src/renderer/src/components/Topbar.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="topbar">
+    <button>katachi</button>
+    <input placeholder="Search fonts" />
+    <button class="menu-button">⋯</button>
+  </div>
+</template>
