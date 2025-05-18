263937a9c47d516135894c84037d2083e7d6d447
mishashto <me@reze.moe>
2025-05-18 14:01:44 +0900

    chore: add basic styling for font displays

--- src/renderer/src/components/FontsBrowser.vue
+++ src/renderer/src/components/FontsBrowser.vue
@@ -21,13 +21,16 @@ onMounted(() => {
     <span>{{ fonts.length }} fonts {{ _fonts }}</span>
     <div class="listing">
       <div v-for="(font, index) in fonts" :key="index">
+        <button>...</button>
+        <span class="font-name">{{ font.fontNames.fullname }}</span>
         <span
           :style="{
             fontFamily: font.fontNames.family,
             fontStyle: font.style.style,
             fontWeight: font.style.weight
           }"
-          >{{ font.fontNames.fullname }}</span
+          class="font-example"
+          >The quick brown fox jumps over the lazy dog.</span
         >
       </div>
     </div>
