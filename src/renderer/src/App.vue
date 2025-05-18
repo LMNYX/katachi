d03f4c052ad9eee423030199eeb13826f7eafb48
mishashto <me@reze.moe>
2025-05-18 14:33:43 +0900

    feat: make search bar finally work

--- src/renderer/src/App.vue
+++ src/renderer/src/App.vue
@@ -1,11 +1,16 @@
 <script setup lang="ts">
+import { ref } from 'vue'
 import Topbar from './components/Topbar.vue'
 import FontsBrowser from './components/FontsBrowser.vue'
 
-const ipcHandle = (): void => window.electron.ipcRenderer.send('ping')
+const filterText = ref('')
+const handleFilterChange = (newFilter: string) => {
+  console.log(newFilter)
+  filterText.value = newFilter
+}
 </script>
 
 <template>
-  <Topbar />
-  <FontsBrowser />
+  <Topbar @update:filter="handleFilterChange" />
+  <FontsBrowser :filter="filterText" />
 </template>
--- src/renderer/src/components/Topbar.vue
+++ src/renderer/src/components/Topbar.vue
@@ -1,9 +1,18 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { defineEmits } from 'vue'
+
+const emit = defineEmits(['update:filter'])
+
+const onInput = (event: Event) => {
+  const target = event.target as HTMLInputElement
+  emit('update:filter', target.value)
+}
+</script>
 
 <template>
   <div class="topbar">
     <button>katachi</button>
-    <input placeholder="Search fonts" />
+    <input placeholder="Search fonts" @input="onInput" />
     <button class="menu-button">⋯</button>
   </div>
 </template>
